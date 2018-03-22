6b4df8a9b03376eb49a7260f03671be604993f82
Arnas Grigaliunas <arnas.grigaliunas@visma.com>
2018-03-22 11:00:22 +0200

    testing: update tests for copy_file function

--- testing/tests/test_file_manager.py
+++ testing/tests/test_file_manager.py
@@ -21,10 +21,10 @@ class TestMoveFile(unittest.TestCase):
             _file.write("Content")
 
         cls.invalid_directory = 55
-        cls.invalid_file = "Test"
+        cls.invalid_file = 'Test'
 
     def test_with_correct_values(self):
-        """Should return new path to the moved file without errors."""
+        """Should return new path to the moved file without any errors."""
         self.assertEqual(fm.move_file(self.test_file, self.test_directory),
                          "{}".format(os.path.join(self.cwd, self.test_directory, self.test_file_name)))
 
@@ -52,35 +52,31 @@ class TestCopyFile(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.cwd = os.getcwd()
-
         cls.test_directory = 'output_folder'
-        cls.test_file_name = 'input_file.txt'
-        cls.test_file = os.path.join(cls.cwd, cls.test_file_name)
-        with open(cls.test_file, 'w') as _file:
-            _file.write("Content")
+        cls.test_file_name = os.path.basename(__file__)
+        cls.test_file = __file__
 
         cls.invalid_directory = 55
-        cls.invalid_file = "Test"
+        cls.invalid_file = 'Test'
 
     def test_with_correct_values(self):
-        """Should return new path to the moved file without errors."""
-        self.assertEqual(fm.move_file(self.test_file, self.test_directory),
-                         "{}".format(os.path.join(self.cwd, self.test_directory, self.test_file_name)))
+        """Should return new path to the copied file without any errors."""
+        self.assertEqual(fm.copy_file(self.test_file, self.test_directory),
+                         "{}".format(os.path.join(os.getcwd(), self.test_directory, self.test_file_name)))
 
     def test_with_invalid_input_file(self):
         """Should raise an IOError exception."""
         with self.assertRaises(IOError):
-            fm.move_file(self.invalid_file, self.test_directory)
+            fm.copy_file(self.invalid_file, self.test_directory)
 
     def test_with_invalid_output_folder(self):
         """Should raise an TypeError exception."""
         with self.assertRaises(TypeError):
-            fm.move_file(self.test_file, self.invalid_directory)
+            fm.copy_file(self.test_file, self.invalid_directory)
 
     @classmethod
     def tearDownClass(cls):
-        shutil.rmtree(cls.test_directory)
+        shutil.rmtree(cls.test_directory, ignore_errors=True)
 
 # if __name__ == '__main__':
 #     unittest.main()
